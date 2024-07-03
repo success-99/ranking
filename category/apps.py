de2cb7c12d97dac6f6493790913db1ed641609a0
mbp13 <shaxobiddina20@gmail.com>
2024-07-03 18:16:38 +0500

    edit signals

--- category/apps.py
+++ category/apps.py
@@ -5,5 +5,5 @@ class HomepageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'category'
 
-    # def ready(self):
-    #     import category.signals
+    def ready(self):
+        import category.signals
